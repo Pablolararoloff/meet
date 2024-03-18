b3a41a0e0e5909b0dbb8bf06f565421146645a75
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2024-03-18 19:46:39 +0100

    notjing to commit

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,7 +4,7 @@ const { google } = require("googleapis"); // Import googleapis
 const calendar = google.calendar("v3"); // Import google calendar v3
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"]; // Define the scopes for the calendar so that we can access the calendar events
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ["https://pablocubo.github.io/meet/"]; // The redirect URI is the URL where the user will be redirected after the authorization
+const redirect_uris = ["https://pablocubo.github.io/meet"]; // The redirect URI is the URL where the user will be redirected after the authorization
 
 
 const oAuth2Client = new google.auth.OAuth2( // Create a new OAuth2Client
