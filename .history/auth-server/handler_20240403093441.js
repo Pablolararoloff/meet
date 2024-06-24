8e6b7d385e0cb561065262f8e205cba723f9818d
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2024-06-24 14:22:23 +0200

    changed uri name in handler.js

--- .history/auth-server/handler_20240403093441.js
+++ .history/auth-server/handler_20240403093441.js
@@ -5,7 +5,7 @@ const { google } =require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ["https://pablolararoloff.github.io/meet/"];
+const redirect_uris = ["https://pablocubo.github.io/meet/"];
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
